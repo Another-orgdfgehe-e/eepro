f43198994ea8b57630f8121995d4ad14d781e7b4
Eric Lacaille <eric.lacaille@gitguardian.com>
2024-02-19 14:57:07 +0100

    deletion commit

--- dingo.py
+++ dingo.py
@@ -1,4 +1,4 @@
-postman= "PMAK-5e3c52895351d62230f49163-3e298b11c641f46dd507a5d91dbbd30183"
+postman= env_var
 # without merge queue and without required
 # without merge queue
 # test now with required
